eef7431650cf982eab9a0524128ec954f08c1884
LeeJoobang <hii5074@gmail.com>
2023-04-18 13:46:45 +0900

    goodlist update

---
+++ GoodList/GoodList/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GoodList
+//
+//  Created by Joobang on 2023/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
